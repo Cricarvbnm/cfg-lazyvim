cd4efcaee2f1168afe080dfd6eb86edcf9c1f93a
Alec <alechron@outlook.com>
2026-02-15 22:57:17 +0800

    feat(theme; options): Catppuccin macchiato; Custom

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local o = vim.opt
+
+o.wrap = true -- Visually newline when needed
+o.listchars = "tab:> <,trail:-,nbsp:+" -- Show some white characters
