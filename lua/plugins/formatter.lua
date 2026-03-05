1ead130d0a198f98806cecab9b6f4226c93bebb3
Alec <alechron@outlook.com>
2026-03-05 16:37:14 +0800

    refactor; feat(formatter): Use a function to add formaaters; JSON

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,9 +1,16 @@
 return {
   "stevearc/conform.nvim",
   opts = function(_, opts)
+    function register_formatter_by_ft(ft, formatter_name)
+      local t = opts.formatters_by_ft[ft] or {}
+      table.insert(t, formatter_name)
+      opts.formatters_by_ft[ft] = t
+    end
+
     -- python
-    local t = opts.formatters_by_ft.python or {}
-    table.insert(t, "black")
-    opts.formatters_by_ft.python = t
+    register_formatter_by_ft("python", "black")
+
+    -- json
+    register_formatter_by_ft("json", "prettier")
   end,
 }
